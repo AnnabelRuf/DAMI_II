749e0616587722b0b73b2d375ef859af00e96999
lufynnca <lf.eckelmann@gmail.com>
2025-01-08 00:26:44 +0100

    Per model a file for execution

---
+++ logistic_regression.py
@@ -0,0 +1,22 @@
+from evaluate import evaluate_model
+from load_motif_data import load_dataset
+
+from sklearn.linear_model import LogisticRegression
+
+
+if __name__ == "__main__":
+
+
+    # Train-test split
+    X_train, y_train, X_test, y_test = load_dataset("GM12878")
+
+    # Model to train and evaluate
+    LR_configs = {
+        'Standard' : LogisticRegression(max_iter=1000, random_state=42)
+    }
+
+
+    for config_name, model in LR_configs.items():
+        #Train model
+        model.fit(X_train, y_train)
+        evaluate_model(model=model, name="RF", config_name=config_name, X_train=X_train, X_test=X_test, y_test=y_test, output_dir=f"Random_Forest_output")
---
+++ random_forest.py
@@ -0,0 +1,23 @@
+from evaluate import evaluate_model
+from load_motif_data import load_dataset
+
+from sklearn.ensemble import RandomForestClassifier
+
+
+
+if __name__ == "__main__":
+
+
+    # Train-test split
+    X_train, y_train, X_test, y_test = load_dataset("GM12878")
+
+    # Model to train and evaluate
+    RF_configs = {
+        'Standard': RandomForestClassifier(random_state=42),
+    }
+
+
+    for config_name, model in RF_configs.items():
+        #Train model
+        model.fit(X_train, y_train)
+        evaluate_model(model=model, name="RF", config_name=config_name, X_train=X_train, X_test=X_test, y_test=y_test, output_dir=f"Random_Forest_output")
