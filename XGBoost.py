5d14ed57159bb05cc5342610343bc521778f9126
lufynnca <lf.eckelmann@gmail.com>
2025-01-08 16:31:47 +0100

    XGBoost shap values and accuracies for both models

--- XGBoost.py
+++ XGBoost.py
@@ -11,5 +11,10 @@ if __name__ == "__main__":
     # Train-test split
     X_train, y_train, X_test, y_test = load_dataset("GM12878")
     model = XGBClassifier()
-    model.fit(X_train, y_train)
-    evaluate_model(model=model, name="XGB", config_name="Standard", X_train=X_train, X_test=X_test, y_test=y_test,output_dir="XGB_outputx")
+    LR_configs = {
+        'Standard' : XGBClassifier(),
+        "Best_Config" : XGBClassifier(eval_metric='mlogloss', learning_rate =  0.1, max_depth= 3, subsample = 1)
+    }
+    for config_name, model in LR_configs.items():
+        model.fit(X_train, y_train)
+        evaluate_model(model=model, name="XGB", config_name=config_name, X_train=X_train, X_test=X_test, y_test=y_test,output_dir="XGB_output")
