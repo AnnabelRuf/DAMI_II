817147389939bb90a4a2608454a7b71426bdee2b
lucafynn <lf.eckelmann@gmail.com>
2025-01-11 19:32:37 +0100

    Removed Dropout and added two aprameter sets

--- NN.py
+++ NN.py
@@ -5,14 +5,11 @@ import sys
 from load_motif_data import load_dataset
 from evaluate import evaluate_model
 
-def create_model(size_l1, size_l2, size_l3,dropout):
+def create_model(size_l1, size_l2, size_l3):
     model = tf.keras.Sequential([
         Dense(size_l1, activation='relu'),
-        Dropout(dropout),
         Dense(size_l3, activation='relu'),
-        Dropout(dropout),
         Dense(size_l3, activation='relu'),
-        Dropout(dropout),
         Dense(1, activation='sigmoid')
     ])
     model.compile(optimizer="adam", 
@@ -21,7 +18,11 @@ def create_model(size_l1, size_l2, size_l3,dropout):
     return model
 
 if __name__ == "__main__":
-    model = create_model(128,64,16,0.5)
+    NN_params = {
+        "Small": [128, 64, 32],
+        "Big": [256, 128, 64]
+    }
+    model = create_model(128,64,64,0.5)
     cell_line = sys.argv[1]
     # Train-test split
     X_train, y_train, X_test, y_test = load_dataset(cell_line)
