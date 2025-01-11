3496189384e3065507265b15ceb0d1fab4eebca4
lucafynn <lf.eckelmann@gmail.com>
2025-01-11 19:40:46 +0100

    NN param configs

--- NN.py
+++ NN.py
@@ -19,12 +19,12 @@ def create_model(size_l1, size_l2, size_l3):
 
 if __name__ == "__main__":
     NN_params = {
-        "Small": [128, 64, 32],
-        "Big": [256, 128, 64]
+        "Small": create_model(128,64,32),
+        "Big": create_model(256,128,64)
     }
-    model = create_model(128,64,64,0.5)
     cell_line = sys.argv[1]
     # Train-test split
     X_train, y_train, X_test, y_test = load_dataset(cell_line)
-    model.fit(X_train,y_train,  epochs = 30, batch_size = 8)
-    evaluate_model(model, "NN", "Standard", X_train, X_test, y_test, output_dir=f"NN_output/{cell_line}", sample_size=500)
+    for name, model in NN_params.items():
+        model.fit(X_train,y_train,  epochs = 30, batch_size = 8)
+        evaluate_model(model, "NN", name, X_train, X_test, y_test, output_dir=f"NN_output/{cell_line}", sample_size=500)
